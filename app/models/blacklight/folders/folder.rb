67b4e6296cac01819b2bf0186c9362c5d8729927
val99erie <val99erie@gmail.com>
2014-10-17 15:10:26 -0500

    Validate name.  Part of #24

--- app/models/blacklight/folders/folder.rb
+++ app/models/blacklight/folders/folder.rb
@@ -2,5 +2,6 @@ module Blacklight::Folders
   class Folder < ActiveRecord::Base
     belongs_to :user, polymorphic: true
     validates :user_id, presence: true
+    validates :name, presence: true
   end
 end
--- spec/models/blacklight/folders/folder_spec.rb
+++ spec/models/blacklight/folders/folder_spec.rb
@@ -1,13 +1,26 @@
 require 'rails_helper'
 
 describe Blacklight::Folders::Folder do
-  let(:user) { FactoryGirl.create(:user) }
+
+  let(:subject) { FactoryGirl.build(:folder) }
+
+  it 'a factory-created folder is valid' do
+    folder = FactoryGirl.build(:folder)
+    expect(folder.valid?).to eq true
+  end
 
   it 'requires a user' do
+    expect(subject.valid?).to eq true
+    subject.user = nil
     expect(subject.valid?).to eq false
     expect(subject.errors.messages[:user_id].first).to match /blank/
-    subject.user = user
+  end
+
+  it 'requires a name' do
     expect(subject.valid?).to eq true
+    subject.name = nil
+    expect(subject.valid?).to eq false
+    expect(subject.errors.messages[:name].first).to match /blank/
   end
 
 end
