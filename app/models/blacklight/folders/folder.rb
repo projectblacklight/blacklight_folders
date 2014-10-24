80ed8f41288f8b2cb98cd97d5ceeac7f081b7743
val99erie <val99erie@gmail.com>
2014-10-24 10:45:28 -0500

    In the form to add a document to a folder, only select folders that are owned by the user and don't already contain the document.  Part of story #11

--- app/models/blacklight/folders/folder.rb
+++ app/models/blacklight/folders/folder.rb
@@ -5,5 +5,14 @@ module Blacklight::Folders
     validates :name, presence: true
 
     has_many :items, -> { order('position ASC') }, class_name: 'FolderItem', :dependent => :destroy
+
+    # Find the folders that belong to this user and don't contain this document
+    def self.without_doc_for_user(document, user)
+      selection = "SELECT blacklight_folders_folders.id AS id, blacklight_folders_folders.name AS name from blacklight_folders_folders"
+      query_to_match_doc_id = "(SELECT * from blacklight_folders_folder_items WHERE blacklight_folders_folder_items.document_id = '#{document.id}')"
+      query = "#{selection} LEFT OUTER JOIN #{query_to_match_doc_id} AS MATCHES_DOC_ID ON blacklight_folders_folders.id = MATCHES_DOC_ID.folder_id WHERE MATCHES_DOC_ID.document_id IS NULL AND blacklight_folders_folders.user_id = #{user.id}"
+      find_by_sql(query)
+    end
+
   end
 end
--- spec/models/blacklight/folders/folder_spec.rb
+++ spec/models/blacklight/folders/folder_spec.rb
@@ -32,4 +32,32 @@ describe Blacklight::Folders::Folder do
     expect(subject.items).to eq [item_B, item_A]
   end
 
+  describe '.without_doc_for_user' do
+    let!(:my_folder)   { FactoryGirl.create(:folder) }
+    let!(:your_folder) { FactoryGirl.create(:folder) }
+    let!(:me)  { my_folder.user }
+    let!(:you) { your_folder.user }
+
+    let(:doc) { SolrDocument.new(id: '12345') }
+    let!(:my_item) { FactoryGirl.create(:item, folder: my_folder) }
+    let!(:your_item) { FactoryGirl.create(:item, folder: your_folder) }
+
+    it 'finds only my folders' do
+      mine = Blacklight::Folders::Folder.without_doc_for_user(doc, me)
+      yours = Blacklight::Folders::Folder.without_doc_for_user(doc, you)
+      expect(mine).to eq [my_folder]
+      expect(yours).to eq [your_folder]
+    end
+
+    it "finds only the folders that don't contain the doc" do
+      my_folder_with_doc = FactoryGirl.create(:folder, user: me)
+      my_item_with_doc = FactoryGirl.create(:item, folder: my_folder_with_doc, document: doc)
+      my_folders = Blacklight::Folders::Folder.where(user_id: me.id)
+      expect(my_folders.map(&:id).sort).to eq [my_folder.id, my_folder_with_doc.id].sort
+
+      result = Blacklight::Folders::Folder.without_doc_for_user(doc, me)
+      expect(result).to eq [my_folder]
+    end
+  end
+
 end
