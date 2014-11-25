8fcc31ea8444aa21b9c7469b4b3fe8e9a83dbf41
Justin Coyne <justin@curationexperts.com>
2014-11-24 20:43:31 -0600

    Update generated initializer for latest Blacklight

--- lib/generators/blacklight_folders/templates/blacklight_folders.rb
+++ lib/generators/blacklight_folders/templates/blacklight_folders.rb
@@ -3,9 +3,9 @@ CatalogController.blacklight_config.navbar.partials.delete(:bookmark)
 # Remove bookmark from catalog#index
 CatalogController.blacklight_config.index.document_actions.delete(:bookmark)
 # Remove bookmark from catalog#show
-CatalogController.document_actions.delete(:bookmark)
+CatalogController.blacklight_config.show.document_actions.delete(:bookmark)
 
 
 CatalogController.add_index_tools_partial(:folder, partial: 'blacklight/folders/folder_control')
-CatalogController.add_document_action(:folder, partial: 'blacklight/folders/show/add_to_folder', if: Proc.new { |ctx| ctx.current_ability.can?(:create, Blacklight::Folders::Folder) } )
+CatalogController.add_show_tools_partial(:folder, partial: 'blacklight/folders/show/add_to_folder', if: Proc.new { |ctx| ctx.current_ability.can?(:create, Blacklight::Folders::Folder) } )
 CatalogController.add_nav_action(:folder, partial: 'blacklight/nav/folders', if: Proc.new { |ctx| ctx.current_ability.can?(:index, Blacklight::Folders::Folder)  } )
