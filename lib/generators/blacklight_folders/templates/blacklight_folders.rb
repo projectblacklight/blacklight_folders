02676e399d5406919f5a0564a69fa5d3e22bc931
Justin Coyne <justin@curationexperts.com>
2014-12-01 13:47:06 -0600

    Update to the latest version of blacklight

--- lib/generators/blacklight_folders/templates/blacklight_folders.rb
+++ lib/generators/blacklight_folders/templates/blacklight_folders.rb
@@ -6,6 +6,7 @@ CatalogController.blacklight_config.index.document_actions.delete(:bookmark)
 CatalogController.blacklight_config.show.document_actions.delete(:bookmark)
 
 
-CatalogController.add_index_tools_partial(:folder, partial: 'blacklight/folders/folder_control')
+CatalogController.add_results_document_tool(:folder, partial: 'blacklight/folders/folder_control')
+CatalogController.add_results_collection_tool(:folder, partial: 'blacklight/folders/add_to_folder')
 CatalogController.add_show_tools_partial(:folder, partial: 'blacklight/folders/show/add_to_folder')
 CatalogController.add_nav_action(:folder, partial: 'blacklight/nav/folders', if: Proc.new { |ctx| ctx.current_ability.can?(:index, Blacklight::Folders::Folder)  } )
